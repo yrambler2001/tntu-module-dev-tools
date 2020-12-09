56a03be472a4524feea1d557cad049176b478827
Yuriy <yrambler2001@gmail.com>
2020-12-09 21:04:53 +0200

    first version

---
+++ index.js
@@ -0,0 +1,18 @@
+eval(atob('KCgoKSA9PiB7CiAgT2JqZWN0LnZhbHVlcyhnZXRFdmVudExpc3RlbmVycyhkb2N1bWVudCkpLm1hcCgodikgPT4gdikuZmxhdCgpLm1hcCgoZikgPT4gW2YudHlwZSwgZi5saXN0ZW5lciwgZi5saXN0ZW5lci50b1N0cmluZygpXSkKICAgIC5maWx0ZXIoKFt0eXBlLCBmLCBzdHJdKSA9PiBzdHIuaW5jbHVkZXMoJ3ByZXZlbnRTZWxlY3Rpb24nKSB8fCBzdHIuaW5jbHVkZXMoJ2tpbGxDdHJsQScpKQogICAgLmZvckVhY2goKFt0eXBlLCBmXSkgPT4gZG9jdW1lbnQucmVtb3ZlRXZlbnRMaXN0ZW5lcih0eXBlLCBmKSk7CgogIGpRdWVyeSh3aW5kb3cpLm9mZignYmx1cicpOwogIGNsZWFySW50ZXJ2YWwodGltZXIpOwogIHdpbmRvdy5zZXRUaW1lb3V0KCgpID0+IHsgY2xlYXJJbnRlcnZhbCh0aW1lcik7IH0sIDEwMCk7CiAgd2luZG93LnNldEludGVydmFsKCgpID0+IHsKICAgIGJsdXJfdGltZSA9IDA7CiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCJmb3JtW25hbWU9J3Rlc3QnXSA+IGlucHV0W25hbWUqPSdidCddIikudmFsdWUgPSAnMCc7CiAgfSwgMTAwMCk7CiAgcmV0dXJuICdvayc7Cn0pKCkpOw=='))
+
+
+
+// ((() => {
+//   Object.values(getEventListeners(document)).map((v) => v).flat().map((f) => [f.type, f.listener, f.listener.toString()])
+//     .filter(([type, f, str]) => str.includes('preventSelection') || str.includes('killCtrlA'))
+//     .forEach(([type, f]) => document.removeEventListener(type, f));
+
+//   jQuery(window).off('blur');
+//   clearInterval(timer);
+//   window.setTimeout(() => { clearInterval(timer); }, 100);
+//   window.setInterval(() => {
+//     blur_time = 0;
+//     document.querySelector("form[name='test'] > input[name*='bt']").value = '0';
+//   }, 1000);
+//   return 'ok';
+// })());
